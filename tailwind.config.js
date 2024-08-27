522e489060e6e1362ebdc255a927ab3517d0f4f0
Aayush Thakur <ashcold21@gmail.com>
2024-08-27 20:09:57 +0530

    Initial Setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography, daisyui],
+}
+
