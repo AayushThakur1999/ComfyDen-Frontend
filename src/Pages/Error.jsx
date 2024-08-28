034e9394e4e438f91be3d80fb29a631ae2a6aa9f
Aayush Thakur <ashcold21@gmail.com>
2024-08-28 17:01:17 +0530

    Fix route structure, add error handling logic to Error.jsx
    ** Updates
    - Added errorElement for login and register routes
    - Add a rule to eslint config file to disable 'react/no-unescaped-entities' errors when ' is used.
    - Update Error.jsx by adding error handling logic

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,6 +1,33 @@
+import { Link, useRouteError } from "react-router-dom";
+
 const Error = () => {
+  const error = useRouteError();
+  console.log(error);
+  if (error.status === 404) {
+    return (
+      <main className="grid place-items-center min-h-screen px-8">
+        <div className="text-center">
+          <p className="text-9xl text-primary font-semibold">404</p>
+          <h1 className="text-3xl font-bold tracking-tight sm:text-5xl mt-4">
+            page not found
+          </h1>
+          <p className="text-lg mt-6">
+            Sorry, we couldn't find the page you're looking for.
+          </p>
+          <div className="mt-10">
+            <Link to="/" className="btn btn-secondary">
+              back home
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
   return (
-    <h1 className="text-4xl font-bold">Error</h1>
-  )
-}
-export default Error
+    <main className="grid place-items-center min-h-screen px-8">
+      <h4 className="text-center text-4xl font-bold">there was an error...</h4>
+    </main>
+  );
+};
+export default Error;
